188d307ecb2bc52e3f1b8d2fd347c36a47b1630b
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-06-04 16:04:14 +0600

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar = () => {
+  return (
+    <div className='flex p-4'>
+      <div className='rounded-2xl bg-[#BEF2C6] text-xl text-black p-2 italic border border-gray-400 font-medium'>
+        <h1>MarathonOps</h1>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
