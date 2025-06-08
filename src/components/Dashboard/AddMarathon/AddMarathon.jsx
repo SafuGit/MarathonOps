11ad382f35183137ed3d6d7a015290c96bf9788e
SafuGit <safwan55.ah@gmail.com>
2025-06-08 15:06:53 +0600

    add react datepicker

--- src/components/Dashboard/AddMarathon/AddMarathon.jsx
+++ src/components/Dashboard/AddMarathon/AddMarathon.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './AddMarathon.css';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
 
 const AddMarathon = () => {
+  const [startRegistrationDate, setStartRegistrationDate] = useState(new Date());
+  const handleStartRegistrationDate = (date) => {
+    setStartRegistrationDate(date);
+    console.log(startRegistrationDate);
+  }
+  const [endRegistrationDate, setEndRegistrationDate] = useState(new Date());
+  const [marathonDate, setMarathonDate] = useState(new Date());
+
   return (
     <div className='w-full flex flex-col items-cente'>
       <div className='w-[90%] rounded-2xl border-2 border-black' id='container'>
         <h1 className='text-3xl text-white text-center mt-4'>Add Marathon ✅</h1>
         <form action="">
-          <div className='grid grid-cols-2 p-4' id='formGrid'>
+          <div className='grid grid-cols-2 p-4 gap-4' id='formGrid'>
             <div className='flex flex-col w-full'>
               <label className='label'>Marathon Title</label>
               <input type="text" name='marathonTitle' placeholder='Marathon Title' className='input rounded-lg' />
@@ -20,6 +30,14 @@ const AddMarathon = () => {
                 <option value={'10k'}>10k</option>
                 <option value={'25k'}>25k</option>
               </select>
+            </div>
+            <div className='flex flex-col w-full'>
+              <label className='label'>Start Registration Date</label>
+              <DatePicker
+                className='select rounded-lg'
+                selected={startRegistrationDate}
+                onChange={(date) => handleStartRegistrationDate(date)}
+              ></DatePicker>
             </div>
           </div>
         </form>
