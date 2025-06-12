cf563a68ed349591b95d78c7e6f0fb8095d62a1a
SafuGit <safwan55.ah@gmail.com>
2025-06-12 14:58:36 +0600

    add dynamic title

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar/Navbar';
-import { Outlet } from 'react-router';
+import { Outlet, useLocation } from 'react-router';
 import Footer from '../components/Footer/Footer';
 
 const Root = () => {
+  const location = useLocation();
+
+  const titles = {
+    '/': 'Home - MarathonOps',
+    '/login': 'Login - MarathonOps',
+    '/register': 'Register - MarathonOps',
+    '/marathons': 'Marathons - MarathonOps',
+    '/dashboard': 'Dashboard - MarathonOps',
+  }
+
+  useEffect(() => {
+    document.title = titles[location.pathname] || 'MarathonOps';
+  }, [location, titles])
+
   return (
     <div>
       <Navbar></Navbar>
