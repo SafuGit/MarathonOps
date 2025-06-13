28b370e35b078f766777cc9512417c46bbafff25
SafuGit <safwan55.ah@gmail.com>
2025-06-13 15:49:43 +0600

    complete registering logic

--- src/components/MarathonRegister/MarathonRegister.jsx
+++ src/components/MarathonRegister/MarathonRegister.jsx
@@ -1,15 +1,49 @@
 import React, { use } from 'react';
 import { useLoaderData } from 'react-router';
 import { AuthContext } from '../../contexts/AuthContext';
+import axios from 'axios';
+import Swal from 'sweetalert2';
 
 const MarathonRegister = () => {
 	const data = useLoaderData();
 	const { user } = use(AuthContext);
+
+	const handleMarathonRegister = (e) => {
+		e.preventDefault();
+		const form = e.target;
+		const [ firstName, lastName, phoneNumber, email ] = [form.firstName, form.lastName, form.phoneNumber, form.email];
+		const registrationData = {
+			marathonId: data._id,
+			firstName: firstName.value,
+			lastName: lastName.value,
+			phoneNumber: phoneNumber.value,
+			email: email.value,
+		}
+		axios.put(`http://localhost:3000/marathons/apply/${data._id}`, registrationData)
+			.then(res => {
+				Swal.fire({
+					icon: 'success',
+					title: 'Success',
+					text: 'You have successfully registered for the marathon!',
+				}).then(() => {
+					form.reset();
+				});
+			})
+			.catch(err => {
+				console.error(err);
+				Swal.fire({
+					icon: 'error',
+					title: 'Oops',
+					text: 'Something went wrong Please try again later',
+				});
+			});
+	}
+
 	return (
 		<div className='w-[50vw] mx-auto my-10 bg-base-300 rounded-3xl p-4'>
 			<h1 className='text-3xl font-extralight'>Registration</h1>
 			<div className='divider divider-vertical mt-0'></div>
-			<form action="" className='flex flex-col gap-4'>
+			<form onSubmit={handleMarathonRegister} className='flex flex-col gap-4'>
 				<div className='flex justify-between items-center-safe'>
 					<p className='text-xl'>Marathon Title</p>
 					<input readOnly disabled className='input rounded-3xl' type="text" placeholder='Marathon Title' defaultValue={data.marathonTitle}/>
