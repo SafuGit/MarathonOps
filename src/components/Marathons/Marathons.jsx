a7e1c56516b77e965e931927bd4742e82e07004e
SafuGit <safwan55.ah@gmail.com>
2025-06-15 18:30:05 +0600

    make marathons responsive

--- src/components/Marathons/Marathons.jsx
+++ src/components/Marathons/Marathons.jsx
@@ -7,8 +7,8 @@ const Marathons = () => {
 	console.log(data);
 	return (
 		<div className='w-full flex flex-col items-center justify-center my-10'>
-			<h1 className='text-5xl font-bold'>MARATHON EVENTS COMING UP INCLUDE</h1>
-			<div className='grid grid-cols-3 mt-4 gap-8'>
+			<h1 className='text-4xl sm:text-5xl text-center font-bold'>MARATHON EVENTS COMING UP INCLUDE</h1>
+			<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 mt-4 gap-8'>
 				{data.map((marathon, index) => (
 					<MarathonCard key={index} data={marathon}></MarathonCard>
 				))}
