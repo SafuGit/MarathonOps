9432a5767178e401b35ce5fa0079895ad0ea083b
Safwan Sadid <98327221+SafuGit@users.noreply.github.com>
2025-06-17 14:55:42 +0600

    update loading color

--- src/components/Loading/Loading.jsx
+++ src/components/Loading/Loading.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Loading = () => {
   return (
-    <div className="fixed inset-0 z-50 bg-green-700 flex items-center justify-center">
+    <div className="fixed inset-0 z-50 bg-sky-300 flex items-center justify-center">
       <span className="loading loading-bars loading-lg text-white"></span>
     </div>
   );
